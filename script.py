ac8abbd7da40b083023a229de41db1014ce96d94
habco123 <114737571+habco123@users.noreply.github.com>
2024-11-27 09:55:05 +0000

    FInal commit

--- script.py
+++ script.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python3
 from time import sleep
 import paramiko
 
-def send_file_via_ssh(local_path, remote_path, remote_host, remote_user, rsa_key_path):
+def setup_ssh_connection(remote_host, remote_user, rsa_key_path):
     # Create an SSH client
     ssh = paramiko.SSHClient()
     
@@ -15,13 +14,11 @@ def send_file_via_ssh(local_path, remote_path, remote_host, remote_user, rsa_key
     # Connect to the remote server using RSA key authentication
     ssh.connect(remote_host, username=remote_user, key_filename=rsa_key_path)
     
+    return ssh
+
+def send_file_via_sftp(sftp, local_path, remote_path):
     # Use SFTP to transfer the file
-    sftp = ssh.open_sftp()
     sftp.put(local_path, remote_path)
-    
-    # Close the SFTP session and SSH connection
-    sftp.close()
-    ssh.close()
 
 # Define the file paths and connection details
 local_file_path = '/var/log/command_log.csv'
@@ -30,7 +27,17 @@ remote_host = '192.168.100.5'
 remote_user = 'user'
 rsa_key_path = '/home/vboxuser/.ssh/id_rsa'
 
-while True:
-    send_file_via_ssh(local_file_path, remote_file_path, remote_host, remote_user, rsa_key_path)
-    sleep(10)
-    print("FIle sent")
+# Setup SSH connection and SFTP session
+ssh = setup_ssh_connection(remote_host, remote_user, rsa_key_path)
+sftp = ssh.open_sftp()
+
+try:
+    while True:
+        send_file_via_sftp(sftp, local_file_path, remote_file_path)
+        sleep(10)
+        print("File sent")
+finally:
+    # Close the SFTP session and SSH connection
+    sftp.close()
+    ssh.close()
+    print("Connection ended")
